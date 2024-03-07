f5ec46138c4b18d3b86fa6ce5ff8afcb8903ccbe
elisepinot <elise.pinot@live.fr>
2024-03-07 13:04:21 +0100

    Ajout fichier .env (sourcemap = false)

--- front-end-argent-bank/src/App.js
+++ front-end-argent-bank/src/App.js
@@ -1,4 +1,4 @@
-import './sass/style.css';
+import './style/style.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import SignIn from './pages/sign-in/SignIn';
